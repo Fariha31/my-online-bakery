395598541bfed2feaca67752711b312a7457b5f3
Fariha31 <62641238+Fariha31@users.noreply.github.com>
2020-08-14 16:34:35 -0700

    b

--- server.js
+++ server.js
@@ -23,4 +23,4 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 const port = process.env.PORT || 5000;
-//app.listen(port, () => console.log(`Connected to port ${port}`));
+app.listen(port, () => console.log(`Connected to port ${port}`));

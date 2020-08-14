921f3e1f2c035a31a77636442463a8dfab74adf4
Fariha31 <62641238+Fariha31@users.noreply.github.com>
2020-08-14 02:00:37 -0700

    yarn_lock

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ const authRoutes = require("./routes/authUser");
 const authProduct = require("./routes/authProduct");
 const authCategory = require("./routes/authCategory");
 const app = express();
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, "client/build")));
-// Anything that doesn't match the above, send back index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
+
 app.use(cors());
 app.use(morgan("dev")); //management
 app.use(express.json()); //incoming data in JSON
@@ -26,5 +21,12 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/products", authProduct);
 app.use("/api/category", authCategory);
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "client/build")));
+// Anything that doesn't match the above, send back index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Connected to port ${port}`));
